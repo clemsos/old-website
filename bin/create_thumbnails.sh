e09c2a46d8aa5d9379007970283c48e575e0ca8d
Clément Renaud <clement.renaud@gmail.com>
2015-11-27 12:11:34 +0100

    clean thumbnails

--- bin/create_thumbnails.sh
+++ bin/create_thumbnails.sh
@@ -14,16 +14,10 @@ do
     do
       IMAGE_TYPE=`file --mime-type -b "$file" | awk -F'/' '{print $1}'`
       if [ x$IMAGE_TYPE = "ximage" ]; then
-        # IMAGE_SIZE=`file -b $file | sed 's/ //g' | sed 's/,/ /g' | awk  '{print $2}'`
-        # WIDTH=`echo $IMAGE_SIZE | sed 's/x/ /g' | awk '{print $1}'`
-        # HEIGHT=`echo $IMAGE_SIZE | sed 's/x/ /g' | awk '{print $2}'`
-        # If the image width is greater that 200 or the height is greater that 150 a thumb is created
-        # if [ $WIDTH -ge  201 ] || [ $HEIGHT -ge 151 ]; then
-            #This line convert the image in a 200 x 150 thumb
           filename=$(basename "$file")
           extension="${filename##*.}"
           filename="${filename%.*}"
-          convert -crop ${THUMB_WIDTH}x${THUMB_HEIGHT}  +repage  +adjoin  "$file" "${content}/thumbnail.png"
+          convert -define size=240x240 "$file" -thumbnail ${THUMB_WIDTH}x${THUMB_HEIGHT}^ -gravity center -extent ${THUMB_WIDTH}x${THUMB_HEIGHT}  "${content}/thumbnail.png"
         #  fi
       fi
     done
